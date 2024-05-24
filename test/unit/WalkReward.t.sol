2f19d2dbd2a42e73b1f47eae0c0895f6d9b3d708
Abhinav Sharma <abhi5237@gmail.com>
2024-05-24 21:27:38 +0530

    In progress

--- test/unit/WalkReward.t.sol
+++ test/unit/WalkReward.t.sol
@@ -5,6 +5,8 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract WalkReward is ERC721, Ownable {
+    uint256 public tokenCounter;
+
     constructor(
         string memory tokenName,
         string memory tokenSymbol
