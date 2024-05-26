cb82dac7a6c99d6f0eb08e1789d831c4d9f9b416
Abhinav Sharma <abhi5237@gmail.com>
2024-05-26 21:46:09 +0530

    file restructure

--- backend/src/WalkReward.sol
+++ backend/src/WalkReward.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "backend/lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "backend/lib/openzeppelin-contracts/contracts/utils/Base64.sol";
+import {Ownable} from "backend/lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract WalkReward is ERC721 {
     //errors
