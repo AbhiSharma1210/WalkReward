ceb9d4a110b201991b7496360d3d76690a49ea05
Abhinav Sharma <abhi5237@gmail.com>
2024-05-29 21:59:38 +0530

    minor update

--- src/WalkReward.sol
+++ src/WalkReward.sol
@@ -1,45 +1,32 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract DiceRoller is VRFConsumerBase {
+contract ClickReward is VRFConsumerBase {
     bytes32 internal keyHash;
     uint256 internal fee;
     uint256 public randomResult;
-    address public owner;
 
-    event DiceRolled(uint256 randomResult);
+    event DiceRolled(uint256 indexed result);
 
     constructor(
-        address vrfCoordinator,
-        address linkToken,
+        address _vrfCoordinator,
+        address _linkToken,
         bytes32 _keyHash,
         uint256 _fee
-    ) VRFConsumerBase(vrfCoordinator, linkToken) {
+    ) VRFConsumerBase(_vrfCoordinator, _linkToken) {
         keyHash = _keyHash;
         fee = _fee;
-        owner = msg.sender;
     }
 
     function rollDice() public returns (bytes32 requestId) {
-        require(
-            LINK.balanceOf(address(this)) >= fee,
-            "Not enough LINK - fill contract with faucet"
-        );
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
         return requestRandomness(keyHash, fee);
     }
 
-    function fulfillRandomness(
-        bytes32 requestId,
-        uint256 randomness
-    ) internal override {
-        randomResult = (randomness % 20) + 1; // To get a number between 1 and 20
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = (randomness % 20) + 1; // Number between 1 and 20
         emit DiceRolled(randomResult);
-    }
-
-    function withdrawLink() external {
-        require(msg.sender == owner, "Only the owner can withdraw");
-        LINK.transfer(msg.sender, LINK.balanceOf(address(this)));
     }
 }
